5c7dfe9aeec8d5b868b9993d7fe8ceb7334076c0
farhanamahmud <farhana.mahmud08@gmail.com>
2017-05-04 04:34:04 +0600

    First Commit

---
+++ button.php
@@ -0,0 +1,33 @@
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,300' rel='stylesheet' type='text/css'></head>
+
+<link rel="stylesheet" type="text/css" href="style.css">
+
+
+
+<body>
+	<div>
+		<iframe src="http://player.vimeo.com/video/99574032?title=0&amp;byline=0&amp;portrait=0&amp;autoplay=1" width="100%" height="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+	</div>
+
+
+	<div class="desktop-footer center">
+		<ul>
+			<li ><a href="index.html" ><img src="logo.png" ></a></li>
+			<li style="padding-top:10px;"><a href="https://itunes.apple.com/us/app/hitch-radio-beyond-music/id875592961?mt=8">IPHONE</a></li>
+			<li style="padding-top:10px;"><a href="https://play.google.com/store/apps/details?id=com.kogi.hitchradio&hl=en" >ANDROID</a></li>
+			<li style="padding-top:10px;"><a href="https://hitchradio.squarespace.com/radiopartners/monthly-fee">RADIO PARTNERS</a></li>
+			<li class="smile_about_us"  style="padding-top:10px;"><a href="about.html" >ABOUT US</a></li>
+		</ul>
+	</div>
+
+
+
+</body>
+</html>
+
+
+
+
+
